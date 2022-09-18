cb72c7921bd46f532dc7b324544e225dc381f534
Thiago Freitas <thiagosistemas3@gmail.com>
2022-09-18 01:10:15 +0000

    melhroias

--- django_kafka/admin.py
+++ django_kafka/admin.py
@@ -1,13 +1,15 @@
-from typing import Dict
+from typing import List
 
 from confluent_kafka.admin import AdminClient, NewTopic
 from django.conf import settings
 
 
-def create_topics(adm : AdminClient, topics : list[str]) -> None:
-    """ Create topics """
+def create_topics(adm: AdminClient, topics: List[str]) -> None:
+    """Create topics"""
 
-    new_topics = [NewTopic(topic, num_partitions=3, replication_factor=1) for topic in topics]
+    new_topics = [
+        NewTopic(topic, num_partitions=3, replication_factor=1) for topic in topics
+    ]
     # Call create_topics to asynchronously create topics, a dict
     # of <topic,future> is returned.
     fs = adm.create_topics(new_topics)
@@ -25,7 +27,9 @@ def create_topics(adm : AdminClient, topics : list[str]) -> None:
 
 
 def setup() -> None:
-    adminClient : AdminClient = AdminClient({'bootstrap.servers': settings.KAFKA_BOOTSTRAP_SERVER})
+    adminClient: AdminClient = AdminClient(
+        {"bootstrap.servers": settings.KAFKA_BOOTSTRAP_SERVER}
+    )
 
     topics: list[str] = [key for key, _ in settings.KAFKA_TOPICS.items()]
 
