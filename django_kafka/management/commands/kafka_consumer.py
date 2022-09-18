e959d8c14bf4da78aad2f21496cfd46abb5b7803
Thiago Freitas <thiagosistemas3@gmail.com>
2022-09-18 03:17:52 +0000

    correção

--- django_kafka/management/commands/kafka_consumer.py
+++ django_kafka/management/commands/kafka_consumer.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from django_kafka.django_kafka.consumer import kafka_consumer_run
+from django_kafka.consumer import kafka_consumer_run
 
 
 class Command(BaseCommand):
